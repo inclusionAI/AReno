f6b96c52e6ec51bcfa86c24304efbcf94a0a8663
jingruzhang625-cmd <jingruzhang625@gmail.com>
2026-07-30 11:02:23 +0800

    feat: add __init__.py for EarlyStopping

---
+++ areno/callbacks/__init__.py
@@ -0,0 +1,8 @@
+"""AReno callbacks module.
+
+This module provides callback classes for extending AReno training functionality.
+"""
+
+from .early_stopping import EarlyStopping
+
+__all__ = ["EarlyStopping"]
